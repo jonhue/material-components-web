66ce85600863151991aa8b57004951f5376f4b53
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-01-10 18:52:35 +0100

    minor bugfixes

--- lib/material_components_web/components.rb
+++ lib/material_components_web/components.rb
@@ -4,24 +4,8 @@ module MaterialComponentsWeb
 
     Mozaic.configure do |config|
         config.define_component 'mdc-form-field'
-        config.define_component 'mdc-checkbox' do
-            defaults = {
-                class_name: false,
-                label: false,
-                checked: false,
-                disabled: false
-            }
-            defaults.merge! options
-        end
-        config.define_component 'mdc-radio' do
-            defaults = {
-                class_name: false,
-                label: false,
-                checked: false,
-                disabled: false
-            }
-            defaults.merge! options
-        end
+        config.define_component 'mdc-checkbox', class_name: false, label: false, checked: false, disabled: false
+        config.define_component 'mdc-radio', class_name: false, label: false, checked: false, disabled: false
     end
 
 end
